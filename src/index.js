1b90912e3825e7591e568ac9565c699dd8c9e31d
Roman Putilov <Putilov.Roman@gmail.com>
2022-01-25 14:45:38 +0500

    Final solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const pairs = new Map(bracketsConfig);  
+  let stack = [];
+  
+  for (let b of str) {    
+    const last = stack[stack.length - 1];
+    
+    if (pairs.get(last) === b) {
+      stack.pop();
+      continue;
+    }
+
+    if (pairs.has(b)) {      
+      stack.push(b);
+      continue;
+    } 
+    return false;
+  }
+    
+  return stack.length === 0;
 }
